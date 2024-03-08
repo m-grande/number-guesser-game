8ccc2b821d4b44e4a96455aa207afd55ea3da189
Michele Grande <125394826+m-grande@users.noreply.github.com>
2024-03-08 17:38:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+function generateTarget() {
+  return Math.floor(Math.random() * 10);
+}
+
+function compareGuesses(humanGuess, computerGuess, secretNumber) {
+  return (
+    Math.abs(humanGuess - secretNumber) <=
+    Math.abs(computerGuess - secretNumber)
+  );
+}
+
+function updateScore(str) {
+  if (str === "human") return humanScore++;
+  if (str === "computer") return computerScore++;
+}
+
+function advanceRound() {
+  return currentRoundNumber++;
+}
